c9fff31d43d0f5f19a0ed1b17d80c3dad469a63c
Giovanna Rodrigues <giovannamendessr@gmail.com>
2021-07-20 15:21:11 -0300

    Initial Commit

---
+++ Selfiegram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Selfiegram
+//
+//  Created by Marcelo Rodrigues de Sousa on 20/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
